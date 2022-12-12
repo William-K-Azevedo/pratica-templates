20f88edfb30c0b1b951a37af2612df687916d2a9
William <williamkorte@hotmail.com>
2022-12-12 12:01:42 -0300

    templates prontos

--- src/templates/ReactQuery/handlers.ts
+++ src/templates/ReactQuery/handlers.ts
@@ -1,8 +1,15 @@
 import axios from "axios";
 
-export const AxiosRequest = async (): Promise<any> => {
+export interface IUsers {
+  id: number;
+  nome: string;
+  password: string;
+  email: string;
+}
+
+export const AxiosRequest = async (): Promise<IUsers[] | undefined> => {
   try {
-    const { data } = await axios.get("https://cataas.com/cat");
+    const { data } = await axios.get("http://localhost:3030");
     return data;
   } catch (error) {
     console.log(error);
--- src/templates/ReactQuery/index.tsx
+++ src/templates/ReactQuery/index.tsx
@@ -1,26 +1,109 @@
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
-import { AxiosRequest } from "./handlers";
+import { AxiosRequest, IUsers } from "./handlers";
 import axios from "axios";
+import { useState } from "react";
+
+interface INovoUser {
+  nome: string;
+  senha: string;
+  email: string;
+}
 
 const ReactQueryTemplates = () => {
+  const [formData, setFormData] = useState<INovoUser>({
+    nome: "",
+    senha: "",
+    email: "",
+  });
   const queryClient = useQueryClient();
 
   // Simples utilização de React Query:
-  const { data, isLoading } = useQuery(["cats"], AxiosRequest);
+  const { data, isLoading } = useQuery(["users"], AxiosRequest);
 
   //   Utilizando a opção select para modificar o dado retornado pela api:
-  const selected = useQuery(["moreCats"], AxiosRequest, {
-    select: (data) => <div>{data}</div>,
-  });
+  // const selected = useQuery(["moreCats"], AxiosRequest, {
+  //   select: (data) => <div>{data}</div>,
+  // });
 
   //Padrão bem utilizado para fazer algo no servidor e em seguida atualizar o estado com o servidor atualizado
-  const myMutation = useMutation(() => axios.post("mutation.url"), {
-    onSuccess: () => {
-      queryClient.invalidateQueries(["cats"]);
-    },
-  });
+  const myMutation = useMutation(
+    (data: INovoUser) => axios.post("http://localhost:3030/", data),
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries(["users"]);
+      },
+    }
+  );
 
-  return <>{isLoading ? <div>Carregando</div> : <div>{data}</div>}</>;
+  return (
+    <>
+      {isLoading ? (
+        <div>Carregando</div>
+      ) : (
+        <div style={{ margin: 10 }}>
+          {data?.map((user) => (
+            <div>
+              <div>ID: {user.id}</div>
+              <div>Nome: {user.nome}</div>
+              <div>Senha: {user.password}</div>
+              <div>Email: {user.email}</div>
+            </div>
+          ))}
+        </div>
+      )}
+      <div>
+        <div>
+          Nome:
+          <input
+            name="nome"
+            onChange={(e) =>
+              setFormData({
+                ...formData,
+                nome: e.target.value,
+              })
+            }
+            type="text"
+            value={formData.nome}
+          />
+        </div>
+        <div>
+          Senha:
+          <input
+            name="senha"
+            onChange={(e) =>
+              setFormData({
+                ...formData,
+                senha: e.target.value,
+              })
+            }
+            type="text"
+            value={formData.senha}
+          />
+        </div>
+        <div>
+          Email:
+          <input
+            name="email"
+            onChange={(e) =>
+              setFormData({
+                ...formData,
+                email: e.target.value,
+              })
+            }
+            type="text"
+            value={formData.email}
+          />
+        </div>
+        <button
+          onClick={() => {
+            myMutation.mutate(formData);
+          }}
+        >
+          Enviar
+        </button>
+      </div>
+    </>
+  );
   //   return <>{isLoading ? <div>Carregando</div> : <div>{data}</div>}</>;
 };
 
